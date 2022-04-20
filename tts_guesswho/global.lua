cd91fac24c9d72eb90c05c570a743e69f74b8db7
ZONT3 <danya10001@gmail.com>
2022-04-20 06:08:29 +0300

    Game script WIP

--- tts_guesswho/global.lua
+++ tts_guesswho/global.lua
@@ -1,3 +1,14 @@
+GAME_ZONE_TL = Vector(-20, 5, -20)
+GAME_ZONE_BR = Vector(20, 5, 20)
+GAME_ZONE_W = 2
+GAME_ZONE_H = 5
+
+PLAYER_ZONES_MAPPING = {
+    ['White'] = 1, ['Brown'] = 2, ['Red'] = 3, 
+    ['Orange'] = 4, ['Yellow'] = 5, ['Green'] = 6, 
+    ['Teal'] = 7, ['Blue'] = 8, ['Purple'] = 9, ['Pink'] = 10
+}
+
 function Vec2f(x, y)
     if type(x) == "userdata" then
         return Vector(x.x, 0, x.z)
@@ -5,9 +16,144 @@ function Vec2f(x, y)
     return Vector(x, 0, y)
 end
 
-GW_GAME = {}
+function Card(guid)
+    local ref = getObjectFromGUID(guid)
+    if not ref then return end
 
+    local meta = {}
+
+    meta.properties = propertiesToTable(ref.getDescription())
+    meta.name = ref.getName()
+    meta.ref_guid = guid
+    meta.ref = ref
+
+    function meta:IsEqual(another)
+        return self.name == another.name
+    end
+
+    return meta
+end
+
+STAGE_PICKING = 1
+STAGE_PLAY = 2
+
+GW_GAME = {}
 GW_GAME.data = {}
+
+function GW_GAME:InitGame()
+    print(' ==== GW Game Init ==== ')
+    self.data.stage = STAGE_PICKING
+    self.data.players = {}
+
+    local names_set = {}
+    local count = 0
+    for _, card in ipairs(self:GetCardList()) do
+        local val = names_set[card.name]
+        if val then
+            if type(val) == "string" then
+                getObjectFromGUID(val).setName(card.name .. ' [1]')
+                val = 1
+            end
+
+            val = val + 1
+            card.ref.setName(card.name .. ' [' .. val .. ']')
+            names_set[card.name] = val
+
+            count = count + 1
+        else
+            names_set[card.name] = card.ref_guid
+        end
+    end
+
+    self.data.player_zones = self:GenerateGridZones(GAME_ZONE_TL, GAME_ZONE_BR, GAME_ZONE_W, GAME_ZONE_H)
+    self.data.game_height = math.max(GAME_ZONE_TL.y, GAME_ZONE_BR.y)
+
+    if count > 0 then
+        print('Fixed ' .. count .. ' name collisions')
+    end
+end
+
+function GW_GAME:InitPlayer(player, card_name)
+    local data = {}
+
+    data.card_name = card_name
+    data.card_states = self:InitCardStates()
+    data.cards_ordered = self:GetCardNamesOrdered(data.card_states)
+    data.displayed = {}
+    data.page = {}
+    data.grid = self:GenerateGridPoints(self.data.player_zones[PLAYER_ZONES_MAPPING[player]])
+
+    self.data.players[player] = data
+end
+
+function GW_GAME:PlayerData(ply)
+    return self.data.players[ply]
+end
+
+function GW_GAME:InitCardStates()
+    local res = {}
+    for _, card in ipairs(self:GetCardList()) do
+        res[card.name] = false
+    end
+    return res
+end
+
+function GW_GAME:GetCardNamesOrdered(card_set)
+    -- TODO
+end
+
+function GW_GAME:FindCard(name)
+    for _, card in ipairs(self:GetCardList()) do
+        if card.name == name then
+            return card
+        end
+    end
+end
+
+function GW_GAME:GetCardList()
+    -- TODO
+end
+
+function GW_GAME:StartGame()
+    self.data.stage = STAGE_PLAY
+    for p, _ in pairs(self.data.players) do
+        self:SetPage(p, 1)
+    end
+end
+
+function GW_GAME:SetPage(ply, idx)
+    local data = self:PlayerData(ply)
+    if idx == 0 then
+        for _, guid in data.displayed do
+            local obj = getObjectFromGUID(guid)
+            if obj then
+                destroyObject(obj)
+            end
+        end
+
+    else
+        local page_len = #data.grid
+        local start = page_len * (idx - 1) + 1
+        local stop = start + page_len - 1
+        local ordered = data.cards_ordered
+
+        self:SetPage(ply, 0)
+        
+        for i = start, stop do
+            if #ordered < i then
+                break
+            end
+
+            local pos = data.grid[i]
+            local card_name = ordered[i]
+            local card = self:FindCard(card_name)
+            local flipped = data.card_states[card_name]
+            
+            -- TODO
+        end
+    end
+    data.page = idx
+end
 
 function GW_GAME:GenerateGridZones(p_tl, p_br, w, h)
     local left = p_tl.x
